47e687be4716ef0cfb18f3c4cf308d62e5be354e
ck9891 <cameronfarquharson1@gmail.com>
2022-01-11 14:21:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-dmwuvs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-dmwuvs)
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import SuperCard from './components/SuperCard';
+import TDAccordion from './components/TDAccordion';
+import TDTable from './components/TDTable';
+
+export default function App() {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ margin: '0 auto', background: '#eee', padding: '7.5vw'}} >
+      <Typography
+        variant="h1"
+        component="div"
+        gutterBottom
+        sx={{ marginBottom: '3rem' }}
+      >
+        MUI Test/Stackblitz offline test
+      </Typography>
+      <SuperCard />
+      <TDAccordion sx={{ marginBottom: '3rem' }} />
+      <TDTable />
+      </Box>
+    </Box>
+  );
+}
